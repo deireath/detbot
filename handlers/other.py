8f2b22a685d030406f95d9cbb2db33ac00ef19d5
deireath <0pospolita0@gmail.com>
2025-09-10 15:11:38 +0700

    create

--- handlers/other.py
+++ handlers/other.py
@@ -1,10 +1,11 @@
+from aiogram import Router
 from aiogram.types import Message
 from lexicon.lexicon import LEXICON_RU
 
 
-from create_dp import dp
+router =Router()
 
-@dp.message()
+@router.message()
 async def send_echo(message: Message):
     try:
         await message.send_copy(chat_id=message.chat_id)
--- main.py
+++ main.py
@@ -3,20 +3,24 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from config.config import Config, load_config
+from handlers import other, user 
 
-from create_dp import dp
 
-
-async def main(dp: Dispatcher) -> None:
+async def main() -> None:
     config: Config = load_config()
+
     logging.basicConfig(
         level=logging.getLevelName(level=config.log.level),
         format=config.log.format,
     )
 
     bot = Bot(token=config.bot.token)
+    dp = Dispatcher()
+
+    dp.include_router(user.router)
+    dp.include_router(other.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-asyncio.run(main(dp))
+asyncio.run(main())
