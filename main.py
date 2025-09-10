6f716abf4a1c1c55e3bd86225ce3300614faa2df
deireath <0pospolita0@gmail.com>
2025-09-10 14:48:51 +0700

    created

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from config.config import Config, load_config
+
+from create_dp import dp
+
+
+async def main(dp: Dispatcher) -> None:
+    config: Config = load_config()
+    logging.basicConfig(
+        level=logging.getLevelName(level=config.log.level),
+        format=config.log.format,
+    )
+
+    bot = Bot(token=config.bot.token)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+asyncio.run(main(dp))
